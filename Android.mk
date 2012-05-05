7b332dbcb6a69c1919decb488c28d9b1fc5ba141
Elliott Hughes <enh@google.com>
2012-05-04 17:07:48 -0700

    Fix zlib-host build.
    Bug: 6369821
    
    Change-Id: I8745b80986deb7dca7395d4f905f09865c3f358d

--- Android.mk
+++ Android.mk
@@ -58,7 +58,7 @@ include $(BUILD_HOST_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
-LOCAL_MODULE := libz
+LOCAL_MODULE := libz-host
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -O3 -DUSE_MMAP
 LOCAL_SRC_FILES := $(zlib_files)
