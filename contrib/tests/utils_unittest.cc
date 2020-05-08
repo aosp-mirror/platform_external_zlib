90fc47e6eed7bd1a59ad1603761303ef24705593
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2020-05-08 03:33:48 +0000

    Adding a test for deflate_stored
    This compression function is used only for copying data from an
    input stream without compression.
    
    Granted, is a user case we don't have on Chromium, but having code
    coverage for it is just a matter of adding a test with compression
    level set to zero.
    
    Bug: 1032721
    Change-Id: I67e84cc34bcadd99671d451a311a05897840595c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2188932
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#766711}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: a50938921e9fb8108bf7b5b25ff3bca95d0b75b9

--- contrib/tests/utils_unittest.cc
+++ contrib/tests/utils_unittest.cc
@@ -66,3 +66,25 @@ TEST(ZlibTest, InflateCover) {
   // cover_trees();
   cover_fast();
 }
+
+TEST(ZlibTest, DeflateStored) {
+  const int no_compression = 0;
+  const zlib_internal::WrapperType type = zlib_internal::WrapperType::GZIP;
+  std::vector<unsigned char> input(1 << 10, 42);
+  std::vector<unsigned char> compressed(
+      zlib_internal::GzipExpectedCompressedSize(input.size()));
+  std::vector<unsigned char> decompressed(input.size());
+  unsigned long compressed_size = static_cast<unsigned long>(compressed.size());
+  int result = zlib_internal::CompressHelper(
+      type, compressed.data(), &compressed_size, input.data(), input.size(),
+      no_compression, nullptr, nullptr);
+  ASSERT_EQ(result, Z_OK);
+
+  unsigned long decompressed_size =
+      static_cast<unsigned long>(decompressed.size());
+  result = zlib_internal::UncompressHelper(type, decompressed.data(),
+                                           &decompressed_size,
+                                           compressed.data(), compressed_size);
+  ASSERT_EQ(result, Z_OK);
+  EXPECT_EQ(input, decompressed);
+}
